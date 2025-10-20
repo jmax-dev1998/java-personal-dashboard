7c744b81f41b14ead9ee3c0cb6cb1f0f45f7af05
jmax-dev1998 <johann2maxwell@gmail.com>
2025-10-21 05:51:39 +0700

    first code java

---
+++ src/main/java/com/dashboard/personal/controller/PersonalController.java
@@ -0,0 +1,14 @@
+package com.dashboard.personal.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class PersonalController {
+    @GetMapping
+    public String tryShowString() {
+        return "SUCCESS DEPLOY PERSONAL DASHBOARD!";
+    }
+}
