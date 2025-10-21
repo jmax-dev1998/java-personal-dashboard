326cc4693af8beea5057d66b58434d82758932a5
jmax-dev1998 <johann2maxwell@gmail.com>
2025-10-21 14:29:38 +0700

    add small edit

--- src/main/java/com/dashboard/personal/controller/DashboardController.java
+++ src/main/java/com/dashboard/personal/controller/DashboardController.java
@@ -10,9 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
-import java.util.Optional;
-
 @Controller
 public class DashboardController {
 
